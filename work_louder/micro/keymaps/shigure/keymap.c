28e10a57c8950f19d8cc9f3f83bc7dc7d0e78bf3
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2024-12-22 16:29:09 -0500

    fix: remove unnecessary code from keymap and rules

--- work_louder/micro/keymaps/shigure/keymap.c
+++ work_louder/micro/keymaps/shigure/keymap.c
@@ -1,26 +1,15 @@
 // Copyright 2022 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include QMK_KEYBOARD_H
-
-// bool is_alt_tab_active = false;
-// uint16_t alt_tab_timer = 0;     
+#include QMK_KEYBOARD_H  
 
 enum custom_keycodes {
     // ALT_TAB = SAFE_RANGE,
     WIN_POS = SAFE_RANGE,
+    LED_LEVEL,
 };
 
 #define WIN_POS G(A(KC_SPACE))
-
-// void matrix_scan_user(void) { // The very important timer.
-//     if (is_alt_tab_active) {
-//         if (timer_elapsed(alt_tab_timer) > 2000) {
-//         unregister_code(KC_LALT);
-//         is_alt_tab_active = false;
-//         }
-//     }
-// }
 
 #ifdef RGB_MATRIX_ENABLE
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max)
@@ -89,24 +78,22 @@ work_louder_config_t work_louder_config;
 
 #define WL_LED_MAX_BRIGHT 75
 
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//     switch (keycode) {
-//         case ALT_TAB:
-//             if (record->event.pressed) {
-//                 if (!is_alt_tab_active) {
-//                     is_alt_tab_active = true;
-//                     register_code(KC_LALT);
-//                 }
-//                 alt_tab_timer = timer_read();
-//                 register_code(KC_TAB);
-//             } else {
-//                 unregister_code(KC_TAB);
-//             }
-//         return false;
-//     default:
-//         return true;
-//     }
-// }
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LED_LEVEL:
+            if (record->event.pressed) {
+                work_louder_config.led_level++;
+                if (work_louder_config.led_level > 4) {
+                    work_louder_config.led_level = 0;
+                }
+                work_louder_micro_led_all_set((uint8_t)(work_louder_config.led_level * 255 / 4));
+                eeconfig_update_user(work_louder_config.raw);
+                layer_state_set_kb(layer_state);
+            }
+            break;
+    }
+    return true;
+}
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -124,85 +111,9 @@ void eeconfig_init_user(void) {
     eeconfig_update_user(work_louder_config.raw);
 }
 
-void keyboard_post_init_user(void) {
+void matrix_init_user(void) {
     work_louder_config.raw = eeconfig_read_user();
-    work_louder_micro_led_all_set((uint8_t)(work_louder_config.led_level * WL_LED_MAX_BRIGHT / 4));
-}
-
-void suspend_wakeup_init_user(void) {
-    layer_state_set_user(layer_state);
-}
-
-
-enum via_indicator_value {
-    id_wl_brightness = 1,
-    id_wl_layer, // placeholder
-};
-
-void wl_config_set_value(uint8_t *data) {
-    // data = [ value_id, value_data ]
-    uint8_t *value_id   = &(data[0]);
-    uint8_t *value_data = &(data[1]);
-
-    switch (*value_id) {
-        case id_wl_brightness:
-            work_louder_config.led_level = (uint8_t)*value_data;
-            work_louder_micro_led_all_set((uint8_t)(work_louder_config.led_level * WL_LED_MAX_BRIGHT / 4));
-            layer_state_set_kb(layer_state);
-            break;
-        // case id_wl_layer:
-        //     layer_move(*value_data);
-        //     break;
-    }
-}
-
-void wl_config_get_value(uint8_t *data) {
-    // data = [ value_id, value_data ]
-    uint8_t *value_id   = &(data[0]);
-    uint8_t *value_data = &(data[1]);
-
-    switch (*value_id) {
-        case id_wl_brightness:
-            *value_data = work_louder_config.led_level;
-            break;
-        // case id_wl_layer:
-        //     *value_data = get_highest_layer(layer_state);
-        //     break;
-    }
-}
-
-void wl_config_save(void) {
-    eeconfig_update_user(work_louder_config.raw);
-}
-
-void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
-    uint8_t *command_id        = &(data[0]);
-    uint8_t *channel_id        = &(data[1]);
-    uint8_t *value_id_and_data = &(data[2]);
-
-    if (*channel_id == id_custom_channel) {
-        switch (*command_id) {
-            case id_custom_set_value: {
-                wl_config_set_value(value_id_and_data);
-                break;
-            }
-            case id_custom_get_value: {
-                wl_config_get_value(value_id_and_data);
-                break;
-            }
-            case id_custom_save: {
-                wl_config_save();
-                break;
-            }
-            default: {
-                // Unhandled message.
-                *command_id = id_unhandled;
-                break;
-            }
-        }
-        return;
-    }
-    *command_id = id_unhandled;
+    work_louder_micro_led_all_set((uint8_t)(work_louder_config.led_level * 255 / 4));
 }
 
 // RGB time out
